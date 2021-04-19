cf0c23d7fb987377ce4f91d318270998aa99cf14
Andres <andresandye@gmail.com>
2021-04-19 13:09:33 -0300

    Comments corrected

---
+++ script.js
@@ -0,0 +1,18 @@
+const clear = document.getElementById("clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const input = document.getElementById("input");
+
+function addToDo(toDo) {
+  const text = (
+    <li class="item">
+      <i class="co fa fa-circle-thin" job="complete"></i>
+      <p class="text"> ${toDo} </p>
+      <i class="de fa fa-trash-o" job="delete"></i>
+    </li>
+  );
+
+  const position = "beforeend";
+  list.insertAdjacentHTML(position, text);
+}
+addToDo("drink coffee");
